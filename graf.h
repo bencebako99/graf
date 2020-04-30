a453f74a41e2ba1c9444b185538c4ee2e1a2c2cc
Bence Bako <bakobence19@gmail.com>
2020-04-30 13:12:34 +0200

    add and remove verteces/edges

--- graf.h
+++ graf.h
@@ -9,6 +9,7 @@ public:
     Matrix<T> M;
 };
 
+//Operators
 template <typename T>
 ostream& operator<<(std::ostream& o, Graf<T> const& G){
     o << 1 << endl;
@@ -49,4 +50,56 @@ istream& operator>>(std::istream& s, Graf<T>& G){
     }
     return s;
 }
+
+//Addig and removing verteces and edges
+template <typename T>
+void add_edge(Graf<T>& G, int i, int j, bool isdirected){
+    G.M(i,j) = 1;
+    if(isdirected == false)
+        G.M(j, i) = 1;
+}
+
+template <typename T>
+void remove_edge(Graf<T>& G, int i, int j, bool isdirected){
+    G.M(i,j) = 0;
+    if(isdirected == false)
+        G.M(j, i) = 0;
+}
+
+template <typename T>
+Graf<T> add_vertex(Graf<T> & G){
+    Graf<T> G1;
+    G1.M.N = G.M.N+1;
+    for(int i=0; i<G1.M.N; i++)
+        for(int j=0; j< G1.M.N; j++)
+            G1.M.data.push_back(0);
+    for(int i=0; i<G.M.N; i++)
+        for(int j=0; j<G.M.N; j++)
+            G1.M(i,j)=G.M(i,j);
+    return G1;
+}
+
+template <typename T>
+Graf<T> remove_vertex(Graf<T> const& G, int x){
+    Graf<T> G1;
+    G1.M.N = G.M.N-1;
+    for(int i=0; i<G1.M.N; i++)
+        for(int j=0; j< G1.M.N; j++)
+            G1.M.data.push_back(0);
+    for(int i=0; i<G1.M.N; i++)
+        if(i<x)
+            for(int j=0; j<G1.M.N; j++)
+                if(j<x)
+                    G1.M(i,j)=G.M(i,j);
+                else
+                    G1.M(i,j) = G.M(i, j+1);
+        else
+            for(int j=0; j<G1.M.N; j++)
+                if(j<x)
+                    G1.M(i,j)=G.M(i+1,j);
+                else
+                    G1.M(i,j) = G.M(i+1, j+1);            
+    return G1;
+}
+
 
--- main.cpp
+++ main.cpp
@@ -10,21 +10,40 @@ int main(int, char**) {
 
     //Testing reading graphs
     {
-    std::ifstream ifile1("graf1.txt");
-    std::ifstream ifile2("graf2.txt");
-    std::stringstream s1, s2;
-    Graf<int> G1, G2;
-    if (ifile1){
-        s1 << ifile1.rdbuf();
-        s1 >> G1;
+        std::ifstream ifile1("graf1.txt"); //adjacency matrix
+        std::ifstream ifile2("graf2.txt"); //adjacency list
+        std::stringstream s1, s2;
+        Graf<int> G1, G2;
+        if (ifile1){
+            s1 << ifile1.rdbuf();
+            s1 >> G1;
+        }
+        if (ifile2){
+            s2 << ifile2.rdbuf();
+            s2 >> G2;
+        }
+        for(int i=0; i<G1.M.N; i++)
+            for(int j=0; j<G1.M.N; j++)
+                if(G1.M(i,j)!=G2.M(i,j)) {cout << "Error in reading graph."; return 0;} //if the read graphs are not the same, there is an error
     }
-    if (ifile2){
-        s2 << ifile2.rdbuf();
-        s2 >> G2;
-    }
-    for(int i=0; i<G1.M.N; i++)
-        for(int j=0; j<G1.M.N; j++)
-            if(G1.M(i,j)!=G2.M(i,j)) {cout << "Error in reading graph."; return 0;}
+    
+    //Test addig and removing verteces and edges
+    {
+        std::ifstream ifile1("graf1.txt"); //adjacency matrix
+        std::stringstream s1;
+        Graf<int> G1;
+        if (ifile1){
+            s1 << ifile1.rdbuf();
+            s1 >> G1;
+        }
+        std::cout << G1;
+        add_edge(G1, 0, 3, false); //add an adge
+        std::cout << G1;
+        remove_edge(G1, 0, 3, false); //remove the edge
+        G1 = add_vertex(G1); //add a vertex
+        std::cout << G1;
+        G1 = remove_vertex(G1, 0); //remove the first vertex
+        std::cout << G1;
     }
 
 }
